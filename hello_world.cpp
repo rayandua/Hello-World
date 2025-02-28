74046e5a24b6c4a40c04c0db18547eaafff1ed9e
Rayan Dua <rayandua1405@gmail.com>
2025-02-27 21:48:12 -0800

    Prints Hello World! in C++

---
+++ hello_world.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+ * This is a simple Hello World program in C++.
+ */
+int main() {
+    // Print Hello World to the terminal
+    cout << "Hello World!" << endl;
+
+    return 0;
+}
