07af7b1d64f290ff556f5ef6dac64962d4a37b35
Rayan Dua <rayandua1405@gmail.com>
2025-02-27 22:06:33 -0800

    Removed the comment, not needed

--- hello_world.cpp
+++ hello_world.cpp
@@ -6,7 +6,7 @@ using namespace std;
  * This is a simple Hello World program in C++.
  */
 int main() {
-    // Print Hello World to the terminal
+    
     cout << "Hello World!" << endl;
 
     return 0;
